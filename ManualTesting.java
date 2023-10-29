7619045f2ce28c4e9052688f12eabf70112a5ea1
sarika lakhe <sarikalakhe1980@yahoo.co.uk>
2023-10-29 15:08:49 +0000

    manual testing1

---
+++ ManualTesting.java
@@ -0,0 +1,18 @@
+package manualtesting;
+
+public class ManualTesting {
+    public static void main(String[] args){
+        // hello world
+                System.out.println("What is software testing");// it is the process to check weather the software is working as per client requirement
+                System.out.println("what is manual testing");
+                System.out.println("what are the challenges of manual testing");
+                System.out.println("what are the 7 principals of software testing");
+                System.out.println("what is test scenario");
+                System.out.println("what is regression testing");
+                System.out.println("what is retesting");
+                System.out.println("what are the software testing technique");
+
+
+
+    }
+}
